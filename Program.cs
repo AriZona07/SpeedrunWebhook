d0efc198f55f9f34edb5b9852957a292a211135f
AriZona07 <ariana.torres.of@gmail.com>
2026-07-17 05:58:44 -0600

    fix: sln arreglado y optimizando imports Se arregló el sln para poder usar "dotnet restore" correctamente. Se quitaron los imports no utilizados.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using DotNetEnv;
 
 public class Program
